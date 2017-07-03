a107145135e7b56608fdea2748bfda6e7ba5f118
Ioanna Mitroulaki <iomitroulaki@yahoo.gr>
2017-07-03 20:07:56 +0200

    Add script files

---
+++ js/myscript.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    
+    //Function, Add new item in the List
+    function addListItem() {
+        var $newListItem,
+            $listItem = $(':text'),
+            $iconDelete = '<span class="glyphicon glyphicon-remove delete"></span>';
+        
+        if ($listItem.val() === '') {
+            alert('You must write something');
+        } else {
+            $newListItem = '<li>' + $listItem.val() + $iconDelete + '</li>';
+            $('ol').append($newListItem);
+            $listItem.val('');
+        }
+    }
+      
+    //Function, Delete an item from the List
+    function deleteItem() {
+        $(this).parent().remove();
+    }
+    
+    //Text decoration for completed tasks
+    function textDecoration() {
+        $(this).css({'text-decoration': 'line-through', 'background-color': '#D3D2D2', 'border-bottom': '1px solid #A9A3A3'});
+    }
+    
+    //Cursor pointer
+    function pointer() {
+        $(this).css('cursor', 'pointer');
+    }
+    
+    $('#btn-add').on('click', addListItem);
+    $(document).on('click', '.delete', deleteItem);
+    $(document).on('click', 'li', textDecoration);
+    $(document).on('mouseover', 'li', pointer);
+	
+	
+});
